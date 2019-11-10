4ce034f7eb5cbccd191b99e77b047a8c522a1066
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2019-11-09 19:47:32 -0500

    Add PropTypes for Source

--- src/components/source.js
+++ src/components/source.js
@@ -1,14 +1,25 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 
 const Source = ({ authors, link, date, formats, topics, title, summary }) => {
-  console.log(title)
+  console.log(authors)
   return (
     <div>
       <h1>{title}</h1>
 
     </div>
   )
+}
+
+Source.propTypes = {
+  authors: PropTypes.array,
+  link: PropTypes.string,
+  date: PropTypes.string,
+  formats: PropTypes.array,
+  topics: PropTypes.array,
+  title: PropTypes.string,
+  summary: PropTypes.string,
 }
 
 export default Source
