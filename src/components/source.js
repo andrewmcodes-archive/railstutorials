ceb46c667403af6b7c3d8da414b85f8424814131
John Espinosa <johnfelix.espinosa@icloud.com>
2019-11-11 18:04:28 -0500

    Source card styling (#5)
    * Fix hero component errors
    
    * Add PropTypes for Source
    
    * add sample sources, style source card
    
    * Flex the sources div

--- src/components/source.js
+++ src/components/source.js
@@ -1,22 +1,32 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 
 const Source = ({ authors, link, date, formats, topics, title, summary }) => {
-  console.log(title)
+  console.log(authors)
   return (
-    <div>
-      <h1>{title}</h1>
 
+    <div className="max-w-2xl m-5 border-solid border-2 p-2 rounded overflow-hidden shadow-lg">
+      <div className="px-6 py-2">
+        <h1 className="font-bold text-xl mb-2"><a href={link}>{title}</a></h1>
+        <h2 className="text-lg">{authors[0]} ・ {date} </h2>
+        <p className="text-gray-700 text-base">
+          {summary}
+        </p>
+      </div>
     </div>
+
   )
 }
 
-export default Source
+Source.propTypes = {
+  authors: PropTypes.array,
+  link: PropTypes.string,
+  date: PropTypes.string,
+  formats: PropTypes.array,
+  topics: PropTypes.array,
+  title: PropTypes.string,
+  summary: PropTypes.string,
+}
 
-//           authors
-//           link
-//           date
-//           formats
-//           topics
-//           title
-//           summary
+export default Source
